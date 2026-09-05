430e240549b1ceb06460bd93c8640155baddb722
Caylusis2 <caylusis2@yahoo.com>
2026-09-05 07:06:36 +0000

    Sep 5, 2026, 7:06 AM

--- testquestion2.js
+++ testquestion2.js
@@ -27,6 +27,6 @@ function draw(){
         ball = new Sprite(mouse.x,mouse.y,10) ;
         ball.collider = 'dynamic';
     }
-    ground = new Sprite(mouse.x, mouse.y,10);
+    ground = new Sprite(mouse.x, mouse.y, 400);
     ground.collider = 'static';
 }
