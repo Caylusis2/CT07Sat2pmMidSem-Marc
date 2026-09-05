91eddcb286d0b4ed4eec2feb1b494852984575bc
Caylusis2 <caylusis2@yahoo.com>
2026-09-05 06:59:42 +0000

    Sep 5, 2026, 6:59 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,18 +1,18 @@
 
-function setup(){
-    createCanvas(600, 400);
-    background(0);
-    world.gravity.y =10;
-}
+// function setup(){
+//     createCanvas(600, 400);
+//     background(0);
+//     world.gravity.y =10;
+// }
 
-function draw(){
-    clear();
-    if(mouse.presses()){
-        fill(random(255),random(255),random(255));
-        ball = new Sprite(mouse.x,mouse.y,10) ;
-        ball.collider = 'dynamic';
-    }
-}
+// function draw(){
+//     clear();
+//     if(mouse.presses()){
+//         fill(random(255),random(255),random(255));
+//         ball = new Sprite(mouse.x,mouse.y,10) ;
+//         ball.collider = 'dynamic';
+//     }
+// }
 
 function setup(){
     createCanvas(600, 400);
