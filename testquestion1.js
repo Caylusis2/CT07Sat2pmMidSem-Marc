3ed59553963309de7959e5e2f3e61de18f87332a
Caylusis2 <caylusis2@yahoo.com>
2026-09-05 06:55:20 +0000

    Sep 5, 2026, 6:55 AM

--- testquestion1.js
+++ testquestion1.js
@@ -6,8 +6,7 @@ function setup(){
 function draw(){
     if(mouse.presses()){
         fill(random(255),random(255),random(255));
-        ball = new Sprite(mouse.x,mouse.y,10) 
-        Sprite(300,200,10);
+        ball = new Sprite(mouse.x,mouse.y,10) ;
         ball.collider = 'dynamic';
     }
 }
